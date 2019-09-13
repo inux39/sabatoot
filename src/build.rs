3c3efb7443b6f27f8e9659f701b0950ef74dab3a
inux39 <inux39@protonmail.ch>
2019-09-13 22:24:24 +0900

    Call git command from path.

--- src/build.rs
+++ src/build.rs
@@ -2,9 +2,10 @@ use std::fs::File;
 use std::io::Write;
 
 fn main() {
+    // TODO: unwrap()
     let out_dir = std::env::var("OUT_DIR").unwrap();
     let path = std::path::Path::new(&out_dir).join("hash.rs");
-    let git = std::process::Command::new("/usr/bin/git")
+    let git = std::process::Command::new("git")
         .args(&["rev-parse", "HEAD"])
         .output().unwrap();
     let hash = String::from_utf8(git.stdout).unwrap();
